059dfb6dea7364c1009683ebe0eaf904db62a8eb
Andres Martin Angulo <monkeyandres@protonmail.com>
2022-12-04 12:44:40 +0100

    feat: day 4 part 1

---
+++ exercises/day04/day04.go
@@ -0,0 +1,57 @@
+package day04
+
+import (
+	"strconv"
+	"strings"
+)
+
+func stringSliceToInt(slice []string) (numbers []int) {
+	for _, item := range slice {
+		num, err := strconv.Atoi(item)
+
+		if err != nil {
+			panic(err)
+		}
+
+		numbers = append(numbers, num)
+	}
+
+	return
+}
+
+func parseInput(input string) (parsedAssignmentPairs [][][]int) {
+	rawAssignmentPairs := strings.Split(input, "\n")
+
+	for _, pair := range rawAssignmentPairs {
+		parsedPair := strings.Split(pair, ",")
+
+		left := stringSliceToInt(strings.Split(parsedPair[0], "-"))
+		right := stringSliceToInt(strings.Split(parsedPair[1], "-"))
+
+		// NOTE: Swap intervals to have the lowest start at index 0
+		if left[0] > right[0] {
+			parsedAssignmentPairs = append(parsedAssignmentPairs, [][]int{right, left})
+		} else if left[0] == right[0] && left[1] < right[1] {
+			parsedAssignmentPairs = append(parsedAssignmentPairs, [][]int{right, left})
+		} else {
+			parsedAssignmentPairs = append(parsedAssignmentPairs, [][]int{left, right})
+		}
+	}
+
+	return
+}
+
+func part1(input string) (overlappingJobsCount int) {
+	assignmentPairs := parseInput(input)
+
+	for _, pair := range assignmentPairs {
+		leftPair := pair[0]
+		rightPair := pair[1]
+
+		if leftPair[0] <= rightPair[0] && leftPair[1] >= rightPair[1] {
+			overlappingJobsCount++
+		}
+	}
+
+	return
+}
---
+++ exercises/day04/day04_test.go
@@ -0,0 +1,41 @@
+package day04
+
+import (
+	"os"
+	"testing"
+)
+
+const exampleInput = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
+
+type testCase struct {
+	input    string
+	expected int
+}
+
+func TestPart1(t *testing.T) {
+	finalInput, err := os.ReadFile("input.txt")
+
+	if err != nil {
+		t.Error("Missing final input file.")
+	}
+
+	cases := []testCase{
+		{input: exampleInput, expected: 2},
+		{input: string(finalInput), expected: 605},
+	}
+
+	for index, v := range cases {
+		result := part1(v.input)
+
+		if result != v.expected {
+			t.Errorf("Expected value %d but received %d", v.expected, result)
+		} else {
+			t.Logf("Test #%d passed!", index)
+		}
+	}
+}
